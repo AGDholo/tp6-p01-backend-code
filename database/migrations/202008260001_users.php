65e45939d882960a28a36c7352d41297ef5641af
agdholo <agdholo@gmail.com>
2020-08-26 20:12:10 +0800

    5.2数据库迁移

---
+++ database/migrations/202008260001_users.php
@@ -0,0 +1,29 @@
+<?php
+use think\migration\Migrator;
+use think\migration\db\Column;
+class Users extends Migrator
+{
+    /**
+     * Migrate Up.
+     */
+    public function up()
+    {
+        // create the table
+        $table = $this->table('users');
+        $table->addColumn('name', 'string')
+            ->addColumn('email', 'string')
+            ->addColumn('password', 'string')
+            ->addColumn('avatar', 'string', ['null' => true, 'default'=>NULL, 'comment'=>'用户头像'])
+            ->addColumn('god', 'boolean', ['default'=>FALSE, 'comment'=>'管理员'])
+            ->addTimestamps('created_at', 'updated_at')
+            ->addIndex('email', ['unique' => true])
+            ->create();
+    }
+    /**
+     * Migrate Down.
+     */
+    public function down()
+    {
+        $this->dropTable('users');
+    }
+}
