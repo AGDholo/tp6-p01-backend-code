2eac7f97019b3a519087cde06a9096101497abae
agdholo <agdholo@gmail.com>
2020-08-31 18:15:17 +0800

    7.6用户登录

--- app/controller/Auth.php
+++ app/controller/Auth.php
@@ -6,12 +6,36 @@ namespace app\controller;
 
 use app\model\User;
 use app\validate\Auth as validateAuth;
+use Exception;
 use think\facade\Request;
 use think\exception\ValidateException;
 use thans\jwt\facade\JWTAuth;
 
 class Auth
 {
+    public function login()
+    {
+        $requestData = Request::post();
+
+        $user = User::where('email', $requestData['email'])->find();
+        
+        if ($user !== null && password_verify($requestData['password'], $user->password)) {
+            return json([
+                'name' => $user->name,
+                'email' => $user->email,
+                'token' => JWTAuth::builder(['id' => $user->id]),
+                'ttl' => env('JWT_TTL')
+            ]);
+        } else {
+            return json(
+                [
+                    'error' => '授权错误，请检查邮件地址或密码'
+                ],
+                401
+            );
+        }
+    }
+
     public function sign()
     {
         $requestData = Request::post();
@@ -32,6 +56,25 @@ class Auth
             return json($data);
         } catch (ValidateException $e) {
             return json($e->getError(), 400);
+        }
+    }
+
+    public function logout()
+    {
+        $authorization = Request::header('Authorization');
+        $token = explode('Bearer ', $authorization)[1];
+        
+        try {
+            JWTAuth::invalidate($token);
+            JWTAuth::validate($token);
+
+            return json([
+                'success' => '登出成功'
+            ]);
+        } catch (Exception $e) {
+            return json([
+                'error' => '登出失败，服务器内部错误'
+            ], 500);
         }
     }
 }
--- route/app.php
+++ route/app.php
@@ -2,3 +2,5 @@
 use think\facade\Route;
 
 Route::post('sign', 'Auth/sign');
+Route::post('login', 'Auth/login');
+Route::post('logout', 'Auth/logout');
